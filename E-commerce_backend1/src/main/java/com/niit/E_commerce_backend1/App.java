b74c3cb7fdb8a04531b798e4d5b7e57ab6e535f8
SANJUKTA SENGUPTA <sanjukta.162@gmail.com>
2017-07-06 11:56:55 +0530

    abc

---
+++ E-commerce_backend1/src/main/java/com/niit/E_commerce_backend1/App.java
@@ -0,0 +1,19 @@
+package com.niit.E_commerce_backend1;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.configuration.Hibernate_config;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	  ApplicationContext context=new AnnotationConfigApplicationContext(Hibernate_config.class);
+       
+    }
+}
