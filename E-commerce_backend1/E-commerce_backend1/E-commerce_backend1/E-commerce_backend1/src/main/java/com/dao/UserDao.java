0be82839e597ce7d2008bd4e0f1a3f0f1bafe08c
SANJUKTA SENGUPTA <sanjukta.162@gmail.com>
2017-07-12 16:50:24 +0530

    change

---
+++ E-commerce_backend1/E-commerce_backend1/E-commerce_backend1/E-commerce_backend1/src/main/java/com/dao/UserDao.java
@@ -0,0 +1,12 @@
+package com.dao;
+
+import java.util.List;
+
+import com.model.User;
+
+public interface UserDao {
+	void saveUser(User user);
+	 List<User>getAllUsers();
+		
+
+}
---
+++ E-commerce_backend1/E-commerce_backend1/E-commerce_backend1/E-commerce_backend1/src/main/java/com/niit/E_commerce_backend1/App.java
@@ -0,0 +1,37 @@
+package com.niit.E_commerce_backend1;
+
+//import org.hibernate.Hibernate;
+
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.configuration.Hibernate_config;
+import com.dao.ProductDaoImpl;
+import com.dao.UserDaoImpl;
+import com.model.Product;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("Hello World !");
+    	  ApplicationContext context=new AnnotationConfigApplicationContext(Hibernate_config.class,ProductDaoImpl.class,UserDaoImpl.class);
+    	//  ProductService productService=(ProductService)context.getBean("productServiceImpl");
+    	  
+    	  
+    	  Product product=new Product();
+    	  product.setProductName("Titian Raga");
+    	  product.setPrice(3000);
+    	  product.setQuantity(20);
+    	  product.setDescription("Titan Raga Mother of Pearl Dial Analog Watch for Women");
+    	  
+    	  // productService.saveProduct(product);
+       
+    }
+}
