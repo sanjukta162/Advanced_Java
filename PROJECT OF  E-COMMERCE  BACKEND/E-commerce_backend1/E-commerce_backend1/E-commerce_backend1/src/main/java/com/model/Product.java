3cde1f2db23f299d5925c787b75f6369060ba77f
SANJUKTA SENGUPTA <sanjukta.162@gmail.com>
2017-07-20 12:21:43 +0530

    ADMIN

---
+++ PROJECT OF  E-COMMERCE  BACKEND/E-commerce_backend1/E-commerce_backend1/E-commerce_backend1/src/main/java/com/model/Product.java
@@ -0,0 +1,85 @@
+package com.model;
+
+
+
+import java.io.Serializable;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+
+
+
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+//import javax.persistence.Transient;
+import javax.validation.constraints.Min;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import org.springframework.stereotype.Component;
+//import org.springframework.web.multipart.MultipartFile;
+@Component
+@Entity
+
+public class Product implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -394001576741655012L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	
+	private int Id;
+	@NotEmpty(message="ProductName cannot be Blank")
+	private String name;
+	@Min(value=50)
+	private double price;
+	private int quantity;
+	@NotEmpty(message ="Product description cannot be blank")
+	private String description;
+	
+	@ManyToOne
+	@JoinColumn(name="cid")
+	private Category category;
+
+	
+	
+//@Transient //it will not get persisted
+	//private MultipartFile image;
+	public int getId() {
+		return Id;
+	}
+	public void setId(int id) {
+		Id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+}
