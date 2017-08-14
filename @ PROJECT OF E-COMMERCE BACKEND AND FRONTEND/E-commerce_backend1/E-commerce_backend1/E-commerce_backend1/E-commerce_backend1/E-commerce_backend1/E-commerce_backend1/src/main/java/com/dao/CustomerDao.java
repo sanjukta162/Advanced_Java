10f9fd42f55950797c9f5bf300485b514e520cf1
SANJUKTA SENGUPTA <sanjukta.162@gmail.com>
2017-08-14 14:52:09 +0530

    14.AUG.2017 UPDATED

---
+++ @ PROJECT OF E-COMMERCE BACKEND AND FRONTEND/E-commerce_backend1/E-commerce_backend1/E-commerce_backend1/E-commerce_backend1/E-commerce_backend1/E-commerce_backend1/src/main/java/com/dao/CustomerDao.java
@@ -0,0 +1,13 @@
+package com.dao;
+
+import com.model.Customer;
+import com.model.User;
+
+public interface CustomerDao {
+	
+	void registerCustomer(Customer customer);
+	User validateUsername(String username);
+	Customer validateEmail(String email);
+	Customer getCustomerByUsername(String username);
+
+}
