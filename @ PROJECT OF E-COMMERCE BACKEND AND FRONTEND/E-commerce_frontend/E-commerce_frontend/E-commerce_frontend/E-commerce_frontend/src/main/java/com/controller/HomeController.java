a1c544b450d5fe8448cf662ea7d28e25fd90232d
SANJUKTA SENGUPTA <sanjukta.162@gmail.com>
2017-08-14 09:16:52 +0530

    13aug2017

---
+++ @ PROJECT OF E-COMMERCE BACKEND AND FRONTEND/E-commerce_frontend/E-commerce_frontend/E-commerce_frontend/E-commerce_frontend/src/main/java/com/controller/HomeController.java
@@ -0,0 +1,70 @@
+package com.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.dao.ProductDao;
+import com.model.Product;
+
+
+
+@Controller
+public class HomeController {
+	@Autowired
+	ProductDao productdao;
+	
+	
+	/* @RequestMapping(value = {"/","/index"})
+	 public String homepage() {
+	        return "index";
+	    }*/
+	
+	
+	/* @RequestMapping("header")*/
+	@RequestMapping(value={"/","/header"})
+	 public String header(){
+		return "header";
+	
+}
+	
+	@RequestMapping("/home")
+	public String home(Model model){
+		List<Product>products=productdao.getAllProducts();
+		model.addAttribute("listProduct",products);
+		return "home";
+	
+}
+	
+	@RequestMapping("aboutus")
+	public String aboutus(){
+		return "aboutus";
+	
+}
+	/*@RequestMapping("Contact")
+	public String Contact(){
+		return "Contact";
+	}*/
+	
+	@RequestMapping("MenCollection")
+	public String MenCollection(){
+		return "MenCollection";
+	}
+
+	
+	@RequestMapping("WomenCollection")
+	public String WomenCollection(){
+		return "WomenCollection";
+	}
+
+	
+	@RequestMapping("footer")
+	public String footer(){
+		return "footer";
+	
+}
+	
+}
